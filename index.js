ccabe86c1ad322e8b19dffd02668d8b2fa441079
nimrod kipngetich <nimrod.kipngetich@student.moringaschool.com>
2025-06-14 22:30:34 +0300

    updates recent commits on index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your code here!
+// Remove the <main id="main">
+const main = document.getElementById('main');
+if (main) {
+  main.remove();
+}
+
+// Create a new <h1> element and assign it to newHeader
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'Nimrod kipngetich is the champion'; // Replace "Your Name" with your actual name
+
+// Append the newHeader to the body
+document.body.appendChild(newHeader);
